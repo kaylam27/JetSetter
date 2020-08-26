d6ed4645d4507fa940db44a1ca8ca3f78fb599fc
Isac Pederson <isacjped@gmail.com>
2020-08-25 20:08:55 -0700

    updated js

--- main2.js
+++ main2.js
@@ -1,34 +1,41 @@
+$(document).ready(function () {
+    $("button").on("click", function () {
 
-
-$("button").on("click", function () {
-
-    var userInput = $("#user-input").val();
-    var settings = {
-        "async": true,
-        "crossDomain": true,
-        "url": "https://us-restaurant-menus.p.rapidapi.com/restaurants/zip_code/" + userInput + "?page=1",
-        "method": "GET",
-        "headers": {
-            "x-rapidapi-host": "us-restaurant-menus.p.rapidapi.com",
-            "x-rapidapi-key": "95cbd7d4b4mshb57f0e4ef6f7096p1b3e47jsn3987729cd66e"
+        var userInput = $("#user-input").val();
+        var settings = {
+            "async": true,
+            "crossDomain": true,
+            "url": "https://us-restaurant-menus.p.rapidapi.com/restaurants/zip_code/" + userInput + "?page=1",
+            "method": "GET",
+            "headers": {
+                "x-rapidapi-host": "us-restaurant-menus.p.rapidapi.com",
+                "x-rapidapi-key": "95cbd7d4b4mshb57f0e4ef6f7096p1b3e47jsn3987729cd66e"
+            }
         }
-    }
-        
-    $.ajax(settings).done(function (response) {
-        var results = response.result.data;
-        // getRestInfo(0, '#restaurants', '#address', '#phone')
-        // function getRestInfo(x,rest,address,phone){
-        for(var i = 0; i<5;i++){
-            $('#restaurants').text(results[i].restaurant_name);
-            $('#address').text(results[i].address.street);
-            $('#phone').text(results[i].restaurant_phone);
-            console.log(results[i]);
-            console.log(i);
-        }
+
+        $.ajax(settings).done(function (response) {
+            var results = response.result.data;
+            for (var i = 0; i < 5; i++) {
+                var restName = $('<h1>');
+                restName.addClass(".rest-name");
+                restName.text(results[i].restaurant_name);
+                $(".restaurant-info").append(restName);
+
+                var restAddress = $('<p>');
+                restAddress.addClass(".rest-add");
+                restAddress.text(results[i].address.street);
+                $(".restaurant-info").append(restAddress);
+
+                var restPhone = $('<p>');
+                restPhone.addClass(".rest-phone");
+                restPhone.text(results[i].restaurant_phone);
+                $(".restaurant-info").append(restPhone);
+
+
+
+            }
+        });
     });
+
 });
-
-
 //  95cbd7d4b4mshb57f0e4ef6f7096p1b3e47jsn3987729cd66e
-
-
