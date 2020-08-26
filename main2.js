7542652705088de6a1beed6f85e3430db1a59251
Isac Pederson <isacjped@gmail.com>
2020-08-25 19:56:26 -0700

    2nd js folder

--- main2.js
+++ main2.js
@@ -32,20 +32,3 @@ $("button").on("click", function () {
 //  95cbd7d4b4mshb57f0e4ef6f7096p1b3e47jsn3987729cd66e
 
 
-$(document).ready(function () {
-
-    var settings = {
-        "async": true,
-        "crossDomain": true,
-        "url": "https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/autosuggest/v1.0/UK/GBP/en-GB/?query=Stockholm",
-        "method": "GET",
-        "headers": {
-            "x-rapidapi-host": "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com",
-            "x-rapidapi-key": "db13f0fa34msh3f267b9d5c647dap1d912bjsn17cd5582b5c0"
-        }
-    }
-
-    $.ajax(settings).done(function (response) {
-        console.log(response);
-    });
-});
