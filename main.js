b7e42c4edbe5c8d259125a7dc99df5efb9bde69e
Isac Pederson <isacjped@gmail.com>
2020-08-25 19:00:35 -0700

    added js file stuff

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 
+
 $("button").on("click", function () {
+
     var userInput = $("#user-input").val();
     var settings = {
         "async": true,
@@ -11,11 +13,21 @@ $("button").on("click", function () {
             "x-rapidapi-key": "95cbd7d4b4mshb57f0e4ef6f7096p1b3e47jsn3987729cd66e"
         }
     }
-
+        
     $.ajax(settings).done(function (response) {
-        console.log(response);
+        var results = response.result.data;
+        // getRestInfo(0, '#restaurants', '#address', '#phone')
+        // function getRestInfo(x,rest,address,phone){
+        for(var i = 0; i<5;i++){
+            $('#restaurants').text(results[i].restaurant_name);
+            $('#address').text(results[i].address.street);
+            $('#phone').text(results[i].restaurant_phone);
+            console.log(results[i]);
+            console.log(i);
+        }
     });
 });
+
 
 //  95cbd7d4b4mshb57f0e4ef6f7096p1b3e47jsn3987729cd66e
 
